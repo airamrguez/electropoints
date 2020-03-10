357edcd9d819f1faa931ff51b9a75c27d4e29e80
Airam Rguez <airamrguezrivero@gmail.com>
2020-03-10 20:01:12 +0100

    Initial Commit

---
+++ ElectroPoints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElectroPoints
+//
+//  Created by Airam Rguez on 10/03/2020.
+//  Copyright © 2020 Airam Rguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
