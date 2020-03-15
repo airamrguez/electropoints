c016cdbea88d6a6d070cfc98f77aca408e14b68d
Airam Rguez <airamrguezrivero@gmail.com>
2020-03-15 12:53:17 +0100

    Step 9

--- ElectroPoints/ListViewController.swift
+++ ElectroPoints/ListViewController.swift
@@ -20,14 +20,26 @@ extension ListViewController: UITableViewDataSource {
         tableView.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("view will appear")
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return PointsService.shared.allPoints.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let point = PointsService.shared.allPoints[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "pointCell") as! ChargingPointCell
-
+        let cell: ChargingPointCell = {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "pointCell") as? ChargingPointCell else {
+                return ChargingPointCell()
+            }
+            return cell
+        }()
+        
         cell.throughfare.text = point.name
         cell.locality.text = point.street
         cell.power.text = String(point.power)
